8154ce86c380ab7558be24cfcdba6ad1c8fbf834
arifmhaque <arif.mmhaque@gmail.com>
2020-10-05 00:38:07 -0400

    project committed.......

---
+++ src/main/java/main/UserDriver.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.openqa.selenium.WebDriver;
+
+public class UserDriver {
+	
+	public WebDriver driver;
+	
+	public UserDriver(WebDriver driver) {
+		this.driver = driver;
+		
+	}
+
+}
---
+++ src/test/java/com/base/BaseTest.java
@@ -0,0 +1,30 @@
+package com.base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+	
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		System.setProperty("webdriver.gecko.driver", "C:/Dev/FireFox/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://www.google.com/");
+	}
+	
+	
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
+	
+
+}
